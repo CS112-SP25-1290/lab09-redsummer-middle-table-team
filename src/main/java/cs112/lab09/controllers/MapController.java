d13c31bccfc5d92de05650fac86d7bbdabb43630
KlowasMunik <Munikgklowas@gmail.com>
2025-04-22 11:42:44 -0700

    map controller done ( if there are issues for implementing other parts using what I did lmk so i can fix it )

--- src/main/java/cs112/lab09/controllers/MapController.java
+++ src/main/java/cs112/lab09/controllers/MapController.java
@@ -1,21 +1,40 @@
 package cs112.lab09.controllers;
 
 import cs112.lab09.Constants;
-import cs112.lab09.RedSummer;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 
-import static cs112.lab09.Constants.*;
-
 public class MapController {
+    @FXML
+    private ImageView mapImageView;
+    @FXML
+    private Button sanFranciscoCaliforniaButton;
+    @FXML
+    private Button bisbeeAzButton;
 
+    @FXML
+    private void handleSanFranciscoButton() throws IOException {
+        System.out.println("clicked San Francisco, California button");
+        openCityPopup(Constants.Event.SAN_FRANCISCO);
+    }
+
+    @FXML
+    private void handleBisbeeButton() throws IOException {
+        System.out.println("clicked Bisbee, Arizona button");
+        openCityPopup(Constants.Event.BISBEE);
+    }
+
+    private void openCityPopup(Constants.Event event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/cs112/lab09/" + Constants.CITY_VIEW_RESOURCE));
+        Scene cityScene = new Scene(loader.load(), 500, 400);
+        Stage popupStage = new Stage();
+        popupStage.setScene(cityScene);
+        popupStage.setTitle(Constants.CITY_VIEW_TITLE);
+        popupStage.show();
+    }
 }
